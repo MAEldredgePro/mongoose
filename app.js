b0e9d054676ed471c9ed3151cd007bd50281161d
Mike Eldredge <maeldredge.pro@gmail.com>
2023-05-16 14:23:02 -0600

    add and retrieve a record

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-
-const mongoose = require('mongoose')
+import mongoose from 'mongoose'
+import Airplane from './model/Airplane.js'
 
 const atlasURI = 'mongodb+srv://maeldredgepro:D0nn3rp05@bender.5udpvf0.mongodb.net/airplanes'
 const localURI = 'mongodb://127.0.0.1:27017/airplanes'
-
 const uri = localURI
-console.log(uri)
-mongoose.connect(uri, {useNewUrlParser: true})
 
-const db = mongoose.connection
+mongoose.connect(uri);
 
-db.once('open', _ => {
-    console.log(`Database connected: ${uri}`)
+const airplane = new Airplane({
+    name: 'DC-3',
+    source: 'Douglas Aircraft',
 })
 
-db.on('error', err => {
-    console.error(`connection error:${err}`)
-})
+await airplane.save()
+
+const firstAirplane = await Airplane.find({});
+console.log(firstAirplane);
