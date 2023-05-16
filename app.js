5f7b955784177d590123610b9e960dc9dc2ec756
Mike Eldredge <maeldredge.pro@gmail.com>
2023-05-16 15:59:55 -0600

    put db connection strings into environment vars

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose'
 import Airplane from './model/Airplane.js'
 
-const atlasURI = 'mongodb+srv://maeldredgepro:D0nn3rp05@bender.5udpvf0.mongodb.net/airplanes'
-const localURI = 'mongodb://127.0.0.1:27017/airplanes'
+const atlasURI = process.env.MONGO_ATLAS
+const localURI = process.env.MONGO_LOCAL
 const uri = localURI
 
-mongoose.connect(uri);
+console.log(`connecting to ${uri}`);
+mongoose.connect(uri)
 
 const airplane = new Airplane({
     name: 'DC-3',
